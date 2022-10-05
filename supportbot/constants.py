89364cb8b31152fe5e6aed75cb9404b54dd0e0c1
andybaumgar <andybaumgar@gmail.com>
2022-10-05 18:12:58 -0400

    Move to dotenv
    Move credentials to .env
    Remove more references to credentails
    Finish switching to dotenv

--- supportbot/constants.py
+++ supportbot/constants.py
@@ -1,7 +1,4 @@
 
-# The path used by the script to look for Slack API tokens
-DEFAULT_CREDENTIALS_PATH = "credentials.json"
-
 # The ID number(s) of the channel to run the bot in
 DEFAULT_CHANNEL_IDS = ["C03TK04U1AL"]
 
