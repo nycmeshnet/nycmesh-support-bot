18b18107009177d440b7e78a369392b1b35b2553
andybaumgar <andybaumgar@gmail.com>
2022-10-05 23:36:39 -0400

    Fix case where no nn found

--- supportbot/request_handler.py
+++ supportbot/request_handler.py
@@ -12,16 +12,16 @@ def handle_support_request(app, config, user_id, channel_id, message_ts):
         text=f"This is a reply to <@{user_id}>! It looks like "
              f"your email is {user.email} {'and' if user.network_number else 'but'} your network number "
              f"{'is ' + str(user.network_number) if user.network_number else 'could not be found'}. "
-             f"Here's a diagnostics report to help our volunteers:",
-    )
-
-    app.client.chat_postMessage(
-        channel=channel_id,
-        thread_ts=message_ts,
-        text=f"Diagnostics running...",
+             f"{'Here is a diagnostics report to help our volunteers:' if user.network_number else ''}"
     )
 
     if user.network_number:
+
+        app.client.chat_postMessage(
+            channel=channel_id,
+            thread_ts=message_ts,
+            text=f"Diagnostics running...",
+        )
 
         report = get_report(user.network_number)
 
