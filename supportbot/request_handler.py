a55b060f08651962e6994bb0a3fab9047393170f
andybaumgar <andybaumgar@gmail.com>
2023-01-21 13:36:31 -0500

    Add NN to network number link

--- supportbot/request_handler.py
+++ supportbot/request_handler.py
@@ -7,15 +7,15 @@ from slack_bolt import App
 
 def nn_to_map_url(nn):
     base = 'https://www.nycmesh.net/map/nodes/'
-    return f'<{base}-{nn}|{nn}>'
+    return f'<{base}-{nn}|NN{nn}>'
 
 def handle_support_request(app, config, user_id, channel_id, message_ts, manual_number=None, at_member = True):
     user = MeshUser(app, user_id, config['nn_property_id'], manual_number=manual_number)
 
     if at_member:
-        text = f"This is a reply to <@{user_id}>! It looks like your email is {user.email} {'and' if user.network_number else 'but'} your network number (NN) {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {'Here is a diagnostics report to help our volunteers:' if user.network_number else ''}"
+        text = f"This is a reply to <@{user_id}>! It looks like your email is {user.email} {'and' if user.network_number else 'but'} your network number {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {'Here is a diagnostics report to help our volunteers:' if user.network_number else ''}"
     else:
-        text = f"The network number (NN) {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {'Here is a diagnostics report to help our volunteers:' if user.network_number else ''}"
+        text = f"The network number {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {'Here is a diagnostics report to help our volunteers:' if user.network_number else ''}"
 
     app.client.chat_postMessage(
         channel=channel_id,
