9c7ae62c9584acf2ec19f791c99b206331094254
andybaumgar <andybaumgar@gmail.com>
2023-02-04 23:06:51 -0500

    Simplify report running message

--- supportbot/supportbot/request_handler.py
+++ supportbot/supportbot/request_handler.py
@@ -14,9 +14,9 @@ def handle_support_request(app, config, user_id, channel_id, message_ts, manual_
     user = MeshUser(app, user_id, config['nn_property_id'], manual_number=manual_number)
 
     if at_member:
-        text = f"This is a reply to <@{user_id}>! It looks like your email is {user.email} {'and' if user.network_number else 'but'} your network number {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {f'Here is a diagnostics report to help our volunteers:{linesep}(The report will appear after approximately 40s.)' if user.network_number else ''}"
+        text = f"This is a reply to <@{user_id}>! It looks like your email is {user.email} {'and' if user.network_number else 'but'} your network number {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {f'Diagnostic report running...' if user.network_number else ''}"
     else:
-        text = f"The network number {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {f'Here is a diagnostics report to help our volunteers:{linesep}(The report will appear after approximately 40s.)' if user.network_number else ''}"
+        text = f"The network number {'is ' + nn_to_map_url(user.network_number) if user.network_number else 'could not be found'}. {f'Diagnostic report running...' if user.network_number else ''}"
 
     app.client.chat_postMessage(
         channel=channel_id,
