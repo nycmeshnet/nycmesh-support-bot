5a085857b49c29de0715132e072f84b1ae068595
andybaumgar <andybaumgar@gmail.com>
2023-02-14 21:28:44 -0500

    Pass in initial_comment

--- supportbot/supportbot/request_handler.py
+++ supportbot/supportbot/request_handler.py
@@ -26,4 +26,4 @@ def handle_support_request(app, config, user_id, channel_id, message_ts, manual_
     if user.network_number:
         report_text = get_report(user.network_number)
 
-        upload_report_file(app, report_text, channel_id, message_ts, user.network_number)
+        upload_report_file(app, report_text, channel_id, message_ts, user.network_number, "Here's a diagnostics report to help our volunteers:")
--- supportbot/supportbot/utils/diagnostics_report.py
+++ supportbot/supportbot/utils/diagnostics_report.py
@@ -1,7 +1,7 @@
 import datetime
 import subprocess
 
-def upload_report_file(app, report_txt, channel_id, thread_id, network_number):
+def upload_report_file(app, report_txt, channel_id, thread_id, network_number, initial_comment):
     timestamp = datetime.datetime.now()
     response = app.client.files_upload(
         channels=channel_id,
@@ -10,7 +10,7 @@ def upload_report_file(app, report_txt, channel_id, thread_id, network_number):
         filename=f"diagnostics_{network_number}_{timestamp.strftime('%Y-%m-%dT%H:%M:%S')}.txt",
         title=f"Diagnostics Report - NN {network_number} on {timestamp.strftime('%Y-%m-%d at %I:%M %p')}",
         thread_ts=thread_id,
-        initial_comment="Here's a diagnostics report to help our volunteers:"
+        initial_comment=initial_comment
     )
     return response['file']['id']
 
