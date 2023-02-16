44e589f5a5106fd93d93bb6ef8be4d365b9fd26a
Andy Baumgartner <andybaumgar@gmail.com>
2023-02-16 00:37:05 -0500

    60 tell user report takes time to run (#70)
    * Add delay message
    
    * Simplify report running message
    
    * WIP
    
    * WIP, add message to file upload
    
    * Remove testing changes
    
    * Pass in initial_comment

--- supportbot/supportbot/utils/diagnostics_report.py
+++ supportbot/supportbot/utils/diagnostics_report.py
@@ -1,7 +1,7 @@
 import datetime
 import subprocess
 
-def upload_report_file(app, report_txt, channel_id, thread_id, network_number):
+def upload_report_file(app, report_txt, channel_id, thread_id, network_number, initial_comment):
     timestamp = datetime.datetime.now()
     response = app.client.files_upload(
         channels=channel_id,
@@ -9,7 +9,8 @@ def upload_report_file(app, report_txt, channel_id, thread_id, network_number):
         filetype='txt',
         filename=f"diagnostics_{network_number}_{timestamp.strftime('%Y-%m-%dT%H:%M:%S')}.txt",
         title=f"Diagnostics Report - NN {network_number} on {timestamp.strftime('%Y-%m-%d at %I:%M %p')}",
-        thread_ts=thread_id
+        thread_ts=thread_id,
+        initial_comment=initial_comment
     )
     return response['file']['id']
 
