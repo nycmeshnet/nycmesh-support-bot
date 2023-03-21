4bd22ae510027c5dfaa42294f9c07231f946108d
andybaumgar <andybaumgar@gmail.com>
2023-03-21 14:10:30 -0400

    Clarify matches variable type

--- supportbot/supportbot/utils/user_data.py
+++ supportbot/supportbot/utils/user_data.py
@@ -47,10 +47,10 @@ class MeshUser:
                 return manual_nn
 
         slack_nn_raw = self._profile.get('fields', {}).get(self._network_number_property_id, None)
-        slack_nn = re.findall("(\d{3,})", slack_nn_raw['value'])
+        slack_nn_matches = re.findall("(\d{3,})", slack_nn_raw['value'])
 
         # slack property nn / install number
-        if slack_nn and (validated_slack_nn := self._database_client.get_nn(int(slack_nn[0]))):
+        if slack_nn_matches and (validated_slack_nn := self._database_client.get_nn(int(slack_nn_matches[0]))):
             return validated_slack_nn
 
         # slack name nn / install number
