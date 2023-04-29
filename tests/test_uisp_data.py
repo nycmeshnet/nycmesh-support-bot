4dd0d81786b0d24a6351c380c17b4803c62144ee
andybaumgar <andybaumgar@gmail.com>
2023-04-28 21:07:18 -0400

    Add files to get UISP data and process nn

---
+++ tests/test_uisp_data.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from supportbot.utils.uisp_data import nn_from_uisp_name, get_uisp_devices
+
+load_dotenv()
+
+def test_get_uisp_devices():
+    assert len(get_uisp_devices()) > 0
+
+def test_nn_from_uisp_name():
+    assert nn_from_uisp_name("lbe-5134-diy-455") == 5134
+    assert nn_from_uisp_name("lbe-5134") == 5134
+    assert nn_from_uisp_name("5134-lbe") == 5134
+
+if __name__ == "__main__":
+    test_nn_from_uisp_name()
