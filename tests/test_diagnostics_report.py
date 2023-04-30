63d2795e0bf7801c96c67bb998d4b77b4754d395
andybaumgar <andybaumgar@gmail.com>
2023-04-30 18:37:01 -0400

    Disable tests until we have test server

--- tests/test_diagnostics_report.py
+++ tests/test_diagnostics_report.py
@@ -9,17 +9,17 @@ load_dotenv()
 # def test_get_uisp_devices():
 #     assert len(get_uisp_devices()) > 0
 
-def test_get_report_lbe_only():
-    report = get_report(690)
-    print(report)
+# def test_get_report_lbe_only():
+#     report = get_report(690)
+#     print(report)
 
-def test_get_report_omni_install():
-    report = get_report(344)
-    print(report)
+# def test_get_report_omni_install():
+#     report = get_report(344)
+#     print(report)
 
-def test_lbe_traceroute_report():
-    report = lbe_traceroute_report('10.96.182.236')
-    print(report)
+# def test_lbe_traceroute_report():
+#     report = lbe_traceroute_report('10.96.182.236')
+#     print(report)
 
-if __name__ == '__main__':
-    test_lbe_traceroute_report()
+# if __name__ == '__main__':
+#     test_lbe_traceroute_report()
