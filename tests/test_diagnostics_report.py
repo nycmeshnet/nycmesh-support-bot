ec3cde8e565967831c02aeb88c5eec8b2fb5b41f
andybaumgar <andybaumgar@gmail.com>
2023-05-04 17:58:07 -0400

    skip tests when unable to run

--- tests/test_diagnostics_report.py
+++ tests/test_diagnostics_report.py
@@ -1,25 +1,23 @@
 import os
 from dotenv import load_dotenv
 from supportbot.utils.diagnostics_report import get_report, lbe_traceroute_report
+import pytest
+import sys
 
 load_dotenv()
+on_mesh = os.environ.get("ON_MESH")
 
-# commented out so test doesn't fail off Mesh
+@pytest.mark.skipif(not on_mesh or sys.platform == "win32", reason="not on mesh or on windows")
+def test_get_report_lbe_only():
+    report = get_report(690)
+    print(report)
 
-# def test_get_uisp_devices():
-#     assert len(get_uisp_devices()) > 0
+@pytest.mark.skipif(not on_mesh or sys.platform == "win32", reason="not on mesh or on windows")
+def test_get_report_omni_install():
+    report = get_report(344)
+    print(report)
 
-# def test_get_report_lbe_only():
-#     report = get_report(690)
-#     print(report)
-
-# def test_get_report_omni_install():
-#     report = get_report(344)
-#     print(report)
-
-# def test_lbe_traceroute_report():
-#     report = lbe_traceroute_report('10.96.182.236')
-#     print(report)
-
-# if __name__ == '__main__':
-#     test_lbe_traceroute_report()
+@pytest.mark.skipif(not on_mesh or sys.platform == "win32", reason="not on mesh or on windows")
+def test_lbe_traceroute_report():
+    report = lbe_traceroute_report('10.96.182.236')
+    print(report)
