c513406f73aeb7d0d7a5d60099a92a7ef94d6cb2
Andy B <andybaumgar@gmail.com>
2023-07-24 17:23:19 -0400

    Remove google maps reference

--- mesh-database-client/mesh_database_client/database.py
+++ mesh-database-client/mesh_database_client/database.py
@@ -8,27 +8,27 @@ from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 import pandas as pd
 import os
-import googlemaps
 from dotenv import load_dotenv
 from numpy import sqrt
 
 load_dotenv()
 
+
 class DatabaseClient:
-
-    def __init__(self, spreadsheet_id = None):
-
+    def __init__(self, spreadsheet_id=None):
         self.spreadsheet_id = spreadsheet_id
         if self.spreadsheet_id is None:
             self.spreadsheet_id = os.environ.get("SPREADSHEET_ID")
 
-        SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
         # created in Google Cloud admin
-        SERVICE_ACCOUNT_FILE = 'credentials.json'
+        SERVICE_ACCOUNT_FILE = "credentials.json"
 
         credentials_dict = self._get_sheets_credentials_from_env()
-        credentials = service_account.Credentials.from_service_account_info(credentials_dict)
-        self.service = build('sheets', 'v4', credentials=credentials)
+        credentials = service_account.Credentials.from_service_account_info(
+            credentials_dict
+        )
+        self.service = build("sheets", "v4", credentials=credentials)
 
         # Call the Sheets API
         self.sheet = self.service.spreadsheets()
@@ -36,7 +36,7 @@ class DatabaseClient:
         self.signup_df = self.get_signup_df()
         self.links_df = self.get_links_df()
 
-        self.gmaps = googlemaps.Client(key=os.environ.get("MAPS_API"))
+        # self.gmaps = googlemaps.Client(key=os.environ.get("MAPS_API"))
 
     def _get_sheets_credentials_from_env(self):
         try:
@@ -49,76 +49,77 @@ class DatabaseClient:
                 "client_id": "GOOGLE_SHEETS_CLIENT_ID",
                 "auth_uri": "GOOGLE_SHEETS_AUTH_URI",
                 "token_uri": "GOOGLE_SHEETS_TOKEN_URI",
-                "auth_provider_x509_cert_url" : "GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL",
-                "client_x509_cert_url" : "GOOGLE_SHEETS_CLIENT_509_CERT_URL"
+                "auth_provider_x509_cert_url": "GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL",
+                "client_x509_cert_url": "GOOGLE_SHEETS_CLIENT_509_CERT_URL",
             }
 
             credentials = {}
             for key, value in credentials_mapping.items():
                 credentials[key] = os.environ.get(value)
 
-            credentials['private_key'] = credentials['private_key'].replace('\\n', '\n')
+            credentials["private_key"] = credentials["private_key"].replace("\\n", "\n")
 
             return credentials
         except Exception as e:
             print(e)
-            raise ValueError('Problem parsing Google Sheets credentials')
-
-
+            raise ValueError("Problem parsing Google Sheets credentials")
 
     def get_range_as_df(self, range):
-        result = self.sheet.values().get(spreadsheetId=self.spreadsheet_id,range=range).execute()
-        values = result.get('values', [])
-        
-        df = pd.DataFrame(values[1:], columns = values[0])
+        result = (
+            self.sheet.values()
+            .get(spreadsheetId=self.spreadsheet_id, range=range)
+            .execute()
+        )
+        values = result.get("values", [])
+
+        df = pd.DataFrame(values[1:], columns=values[0])
         return df
 
     def get_signup_df(self):
-        df = self.get_range_as_df('Form Responses 1!A:AP')
- 
+        df = self.get_range_as_df("Form Responses 1!A:AP")
+
         # force columns to be specific type
-        df['NN'] = (pd.to_numeric(df['NN'], errors="coerce").fillna(0).astype(int))
-        df['ID'] = (pd.to_numeric(df['ID'], errors="coerce").fillna(0).astype(int))
+        df["NN"] = pd.to_numeric(df["NN"], errors="coerce").fillna(0).astype(int)
+        df["ID"] = pd.to_numeric(df["ID"], errors="coerce").fillna(0).astype(int)
 
-        df['Latitude'] = pd.to_numeric(df['Latitude'], errors='coerce')
-        df['Longitude'] = pd.to_numeric(df['Longitude'], errors='coerce')
+        df["Latitude"] = pd.to_numeric(df["Latitude"], errors="coerce")
+        df["Longitude"] = pd.to_numeric(df["Longitude"], errors="coerce")
 
-        df.drop(df.tail(1).index,inplace=True)
-         
+        df.drop(df.tail(1).index, inplace=True)
+
         return df
 
-
     def get_links_df(self):
-        df = self.get_range_as_df('Links!A:I')
+        df = self.get_range_as_df("Links!A:I")
 
         # force columns to be specific type
-        df['to'] = (pd.to_numeric(df['to'], errors="coerce").fillna(0).astype(int))
-        df['from'] = (pd.to_numeric(df['from'], errors="coerce").fillna(0).astype(int))
-        
+        df["to"] = pd.to_numeric(df["to"], errors="coerce").fillna(0).astype(int)
+        df["from"] = pd.to_numeric(df["from"], errors="coerce").fillna(0).astype(int)
+
         return df
 
     def name_to_nn(self, name):
         signup_df = self.signup_df
-        name_df = signup_df[signup_df['Name'].str.contains(name, case=False)]
-        
+        name_df = signup_df[signup_df["Name"].str.contains(name, case=False)]
+
         if name_df.empty:
             return None
-        
+
         entry = name_df.iloc[0]
-        if (nn := entry['NN']) == 0:
+        if (nn := entry["NN"]) == 0:
             return None
         return nn
 
     def email_to_nn(self, email):
         # TODO multiple emails recency
         signup_df = self.signup_df
-        email_df = signup_df[signup_df['Email'].str.contains(email, case=False)]
-        
+        email_df = signup_df[signup_df["Email"].str.contains(email, case=False)]
+
         if email_df.empty:
             return None
 
         entry = email_df.iloc[0]
-        if (nn := entry['NN']) == 0:
+        if (nn := entry["NN"]) == 0:
             return None
         return nn
 
@@ -150,47 +151,45 @@ class DatabaseClient:
 
     #     return closest['NN']
 
-
     def nn_to_linked_nn(self, nn):
         links_df = self.links_df
-        
-        from_df = links_df[links_df['from']==nn]
-        to_df = links_df[links_df['to']==nn]
 
-        from_nns = from_df['to'].tolist() 
-        to_nns = to_df['from'].tolist() 
+        from_df = links_df[links_df["from"] == nn]
+        to_df = links_df[links_df["to"] == nn]
+
+        from_nns = from_df["to"].tolist()
+        to_nns = to_df["from"].tolist()
 
         connected_nodes = from_nns + to_nns
 
         return connected_nodes
 
     def get_nn(self, input_number):
-
         if input_number is None:
             return None
 
         input_number = int(input_number)
 
-        id_rows = self.signup_df[self.signup_df['ID'] == input_number]
+        id_rows = self.signup_df[self.signup_df["ID"] == input_number]
         for index, row in id_rows.iterrows():
-            if row['Status'] == 'NN assigned':
+            if row["Status"] == "NN assigned":
                 return input_number
-            elif row['Status'] == 'Installed':
-                return row['NN']
-        
-        nn_rows = self.signup_df[self.signup_df['NN'] == input_number]
+            elif row["Status"] == "Installed":
+                return row["NN"]
+
+        nn_rows = self.signup_df[self.signup_df["NN"] == input_number]
         for index, row in nn_rows.iterrows():
-            if row['Status'] == 'Installed':
+            if row["Status"] == "Installed":
                 return input_number
 
         return None
 
     def nn_to_location(self, nn):
-        row = self.signup_df[self.signup_df['NN'] == nn].iloc[0]
-        return {'Latitude': row.Latitude, 'Longitude': row.Longitude}
+        row = self.signup_df[self.signup_df["NN"] == nn].iloc[0]
+        return {"Latitude": row.Latitude, "Longitude": row.Longitude}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     database_client = DatabaseClient()
     name = database_client.name_to_nn("test")
     print(name)
