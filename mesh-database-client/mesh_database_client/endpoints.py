a4102dc5bcde9b5e4beb1a1b7d2f8f56aab29ccb
Andrew Dickinson <andrewjd@amazon.com>
2024-01-28 21:24:41 -0500

    Use auth token to communicate with MeshDB

--- mesh-database-client/mesh_database_client/endpoints.py
+++ mesh-database-client/mesh_database_client/endpoints.py
@@ -1,8 +1,8 @@
 import os
 
-MESH_API_ENDPOINT_BASE = os.environ["MESH_API_ENDPOINT_BASE"]
+MESHDB_API_ENDPOINT_BASE = os.environ["MESHDB_API_ENDPOINT_BASE"]
 
 
-MEMBER_LOOKUP_ENDPOINT = os.path.join(MESH_API_ENDPOINT_BASE, "member/lookup/")
-INSTALL_LOOKUP_ENDPOINT = os.path.join(MESH_API_ENDPOINT_BASE, "install/lookup/")
-BUILDINGS_ENDPOINT = os.path.join(MESH_API_ENDPOINT_BASE, "buildings/")
+MEMBER_LOOKUP_ENDPOINT = os.path.join(MESHDB_API_ENDPOINT_BASE, "member/lookup/")
+INSTALL_LOOKUP_ENDPOINT = os.path.join(MESHDB_API_ENDPOINT_BASE, "install/lookup/")
+BUILDINGS_ENDPOINT = os.path.join(MESHDB_API_ENDPOINT_BASE, "buildings/")
