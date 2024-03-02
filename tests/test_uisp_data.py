0174d3548cce554757521e564ae4dfb21a7e3383
Andrew Dickinson <andrewjd@amazon.com>
2024-03-02 11:21:29 -0500

    Convert UISP timestamps to local NYC time

--- tests/test_uisp_data.py
+++ tests/test_uisp_data.py
@@ -1,6 +1,9 @@
 import os
+from datetime import timedelta
+
+from dateutil import tz
 from dotenv import load_dotenv
-from supportbot.utils.uisp_data import nn_from_uisp_name, get_uisp_devices
+from supportbot.utils.uisp_data import nn_from_uisp_name, get_uisp_devices, human_readable_uisp_time
 import pytest
 from distutils.util import strtobool
 
@@ -18,7 +21,18 @@ def test_nn_from_uisp_name():
     assert nn_from_uisp_name("nycmesh-lbe-136") == 136
     assert nn_from_uisp_name("nycmesh-19-omni") == 19
     assert nn_from_uisp_name("nycmesh-3-omni") == 3
-    
+
+def test_uisp_time_is_converted_to_nyc():
+    assert human_readable_uisp_time(
+        "2024-03-01T13:50:06Z",
+        tz.tzoffset("EDT", timedelta(hours=-4))
+    ) == "2024-03-01 09:50:06"
+
+    assert human_readable_uisp_time(
+        "2024-03-01T13:50:06Z",
+        tz.tzoffset("EST", timedelta(hours=-5))
+    ) == "2024-03-01 08:50:06"
+
 
 if __name__ == "__main__":
     test_nn_from_uisp_name()
