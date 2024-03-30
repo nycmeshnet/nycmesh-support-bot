ab043eb65ca7df2d22edd7622a3b8b53513e8751
Andrew Dickinson <andrewjd@amazon.com>
2024-03-30 16:49:23 -0400

    Fix: install numbers don't work correctly & default to user's NN

--- supportbot/supportbot/request_handler.py
+++ supportbot/supportbot/request_handler.py
@@ -1,4 +1,7 @@
+import os
 from webbrowser import get
+
+from mesh_database_client import MeshDBDatabaseClient
 from supportbot.utils.diagnostics_report import upload_report_file, get_report
 from supportbot.utils.user_data import MeshUser
 import subprocess
@@ -9,22 +12,40 @@ def nn_to_map_url(nn):
     base = 'https://www.nycmesh.net/map/nodes/'
     return f'<{base}-{nn}|NN{nn}>'
 
-def handle_support_request(app, config, user_id, channel_id, message_ts, manual_number=None):
-    user = MeshUser(app, user_id, config['nn_property_id'], manual_number=manual_number)
 
-    if not user.network_number:
-        text = (
-            "The Install Number could not be automatically determined.  You can find your "
-            "Install Number by searching the registered email for the Subject "
-            "\"NYC Mesh Rooftop Install.\"  Providing this number will help volunteers locate "
-            "your number in our system to better assist you."
-        )
+def handle_support_request(app, config, user_id, channel_id, message_ts, manual_number=None):
+    database_client = MeshDBDatabaseClient(os.environ.get("MESHDB_AUTH_TOKEN"))
+    user = MeshUser(app, user_id, config['nn_property_id'], database_client=database_client)
+
+    network_number = None
+    if manual_number:
+        manual_nn = database_client.get_nn(int(manual_number))
+        if manual_nn:
+            network_number = manual_nn
+            text = (
+                f"The network number entered was {nn_to_map_url(network_number)}" +
+                (f" (via install #{manual_number})" if int(manual_number) != int(network_number) else "") +
+                f". Diagnostic report running..."
+            )
+        else:
+            text = (
+                f"Entered number: {manual_number} could not be found. Please check the number is a "
+                f"valid install or network number and try again"
+            )
     else:
-        text = (
-            f"The network number {'entered was' if manual_number else ''} "
-            f"{nn_to_map_url(user.network_number) + (' was automatically detected' if not manual_number else '')}. "
-            f"Diagnostic report running..."
-        )
+        if user.network_number:
+            network_number = user.network_number
+            text = (
+                f"The network number {nn_to_map_url(user.network_number)} was automatically detected. "
+                f"Diagnostic report running..."
+            )
+        else:
+            text = (
+                "The Install Number could not be automatically determined.  You can find your "
+                "Install Number by searching the registered email for the Subject "
+                "\"NYC Mesh Rooftop Install.\"  Providing this number will help volunteers locate "
+                "your number in our system to better assist you."
+            )
 
     app.client.chat_postMessage(
         channel=channel_id,
@@ -32,11 +53,11 @@ def handle_support_request(app, config, user_id, channel_id, message_ts, manual_
         text=text
     )
 
-    if user.network_number:
+    if network_number:
         try:
-            report_text = get_report(user.network_number)
+            report_text = get_report(network_number)
 
-            upload_report_file(app, report_text, channel_id, message_ts, user.network_number, "Here's a diagnostics report to help our volunteers:")
+            upload_report_file(app, report_text, channel_id, message_ts, network_number, "Here's a diagnostics report to help our volunteers:")
         except Exception as e:
             app.client.chat_postMessage(
                 channel=channel_id,
