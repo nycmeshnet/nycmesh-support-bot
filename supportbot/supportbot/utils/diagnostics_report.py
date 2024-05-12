8fa84333e722189ff4025f791c95f27c7b0a443f
Andrew Dickinson <andrewjd@amazon.com>
2024-05-12 16:40:11 -0400

    Fix: stop using deprecated file upload API

--- supportbot/supportbot/utils/diagnostics_report.py
+++ supportbot/supportbot/utils/diagnostics_report.py
@@ -14,10 +14,9 @@ def upload_report_file(
     app, report_txt, channel_id, thread_id, network_number, initial_comment
 ):
     timestamp = datetime.datetime.now(tz=timezone("US/Eastern"))
-    response = app.client.files_upload(
-        channels=channel_id,
+    response = app.client.files_upload_v2(
+        channel=channel_id,
         content=report_txt,
-        filetype="txt",
         filename=f"diagnostics_{network_number}_{timestamp.strftime('%Y-%m-%dT%H:%M:%S')}.txt",
         title=f"Diagnostics Report - NN {network_number} on {timestamp.strftime('%Y-%m-%d at %I:%M %p')}",
         thread_ts=thread_id,
