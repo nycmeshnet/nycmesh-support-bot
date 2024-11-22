35606560c1a94af6265424fe4dc231d748247524
Andrew Dickinson <andrewjd@amazon.com>
2024-11-22 15:17:43 -0500

    Configure logger

--- supportbot/supportbot/cli/bot_server.py
+++ supportbot/supportbot/cli/bot_server.py
@@ -1,3 +1,5 @@
+import logging
+
 import supportbot.app
 import click
 
@@ -18,6 +20,12 @@ from supportbot.constants import DEFAULT_CHANNEL_IDS, DEFAULT_NETWORK_NUMBER_PRO
     help="The Slack ID number for the user property containing their network number"
 )
 def main(channel_ids, nn_property_id):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)  # Set log level to INFO
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+    logger.addHandler(handler)
+
     supportbot.app.run_app({
         'channel_ids': channel_ids,
         'nn_property_id': nn_property_id
